c54777994c67e7f7ca96194129850bfaa543c293
admin <admin@mocambos.net>
2012-08-03 22:42:15 -0400

    Primeira versao

---
+++ admin.py
@@ -0,0 +1,8 @@
+from gitannex.models import Baobaxia
+from django.contrib import admin
+
+"""
+Arquivo de configuracao e customizacao da interface administrativa do Django.
+"""
+
+admin.site.register(Baobaxia)
---
+++ models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+import logging
+
+"""
+Modelos da aplicacao Django. 
+
+Neste arquivo sao definidos os modelos de dados da aplicacao *baobaxia*.
+"""
+
+logger = logging.getLogger(__name__)
