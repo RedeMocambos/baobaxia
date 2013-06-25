044d54a5621f433bab5f2fc49dc28ecf757fc1f7
Fernao <fernao@riseup.net>
2013-06-25 16:30:44 -0300

    testes com policies e funcoes iniciais da api.js

---
+++ api.js
@@ -0,0 +1,70 @@
+
+/**
+   @param {file} file File to upload 
+    @return jsonObject Json object with suggested metadata
+*/
+function pre_publish(file){
+    // offline
+    // 1. upload the file
+    // 2. analyze the file
+    suggest_metadata()
+    return jsonObject;
+}
+
+/**
+*/
+function publish(){
+    // offline
+}
+
+function suggest_metadata(){
+    // offline
+    // call suggest_title()
+    // call suggest_autor()
+} 
+
+/**
+   @param {Array} array1 Array of tags to join 
+   @param {Array} array2 Array of tags to join
+   @return jointArray Array with joint tags 
+*/
+function join_tags(array1, array2){
+    // sync
+    return jointArray;
+}
+
+/**
+*/
+function generateForm(){
+    // offline
+    // verifica policy e gera campos do form
+}
+
+
+/**
+   @param {Array} form Array of fields to be checked
+*/
+function validate(form){
+    // offline
+
+    // for each field, check according to policy
+    var dataValidate;
+    var validatePolicy = new XMLHttpRequest();
+    var url = "policies/validation.json";
+    
+    $.getJSON(url, function(data) {
+	for (field in form) {
+	    fieldPolicy = eval("data.metadata[0]." + field);
+	    check_policy(field, fieldPolicy);
+	}
+    });
+    
+}
+
+/**
+   @param {string} fieldName Name of the field
+   @param {string} fieldPolicy Policy applied for this field
+*/
+function check_policy(fieldName, fieldPolicy){
+    // offline
+}
