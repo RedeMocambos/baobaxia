0ba43edc2ab72ade2765bb05785112935ef2a7e2
20 <vince@mocambos.net>
2013-07-03 21:45:54 -0300

    Começando a ver upload.. #38

--- api_node.js
+++ api_node.js
@@ -1,32 +1,29 @@
 var http = require('http');
 var url = require('url');
+// var ecstatic = require('ecstatic');
 
 var express = require('express');
 var app = express();
 app.configure(function() {
     app.use(express.bodyParser()); // used to parse JSON object given in the request body
+//    app.use(ecstatic({ root: __dirname }));
 });
 
 
 /**
- * HTTP GET /api/view:id
- * Param: :id is filepath
+ * HTTP POST /api/view
  * Returns: file metadatas in a JSON format
  * Error: 404 HTTP code if the task doesn't exists
  */
 
-app.post('/api/view', function (request, response) {
-    console.log('>>> Request.params:   ' + request.params);
-    console.log('>>> Request.body:   '+ request.body);
+app.get('/api/view/whereis', function (request, response) {
 
     var path = request.body.path;
-    console.log('>>> Request.body.path:   '+ request.body.path);
+    console.log(path);
     var fileName = request.body.filename;
-    console.log('>>> Request.body.filename:   '+ request.body.filename);
-    
+    console.log(fileName);
+
     try {
-    
-	var spawn = require('child_process').spawn;
 	console.log('Starting directory: ' + process.cwd());
 	try {
 	    process.chdir(path);
@@ -35,25 +32,19 @@ app.post('/api/view', function (request, response) {
 	catch (err) {
 	    console.log('chdir: ' + err);
 	}
-	
-	ga = spawn('git-annex', ['whereis', '--json', fileName, '']);
-	response.setHeader('Content-Type', 'text/json; charset=utf-8');
-	
-	ga.stdout.on('data', function (data) {
-	    response.end(data);
-	});
-	
-	ga.stdout.on('data', function (data) {
-	    console.log('>>> Request.params:   ' + data);
-	});
-	
-	ga.stderr.on('data', function (data) {
-	    console.log('stderr: ' + data);
-	});
-	
-	ga.on('close', function (code) {
-	    console.log('child process exited with code ' + code);
-	});
+
+	var exec = require('child_process').exec,
+	child;
+
+	child = exec('git-annex whereis --json ' + fileName,
+		     function (error, stdout, stderr) {
+			 console.log(stdout);
+			 response.end(stdout)
+			 console.log(stderr);
+			 if (error !== null) {
+			     console.log('exec error: ' + error);
+			 }
+		     });
 
     } catch (exception) {
         response.send(404);
@@ -61,4 +52,88 @@ app.post('/api/view', function (request, response) {
      
 });
 
+
+app.post('/api/publish', function (request, response){
+
+    pre_publish()
+    
+    try{
+	console.log('Starting directory: ' + process.cwd());
+	try {
+	    process.chdir(path);
+	    console.log('New directory: ' + process.cwd());
+	}
+	catch (err) {
+	    console.log('chdir: ' + err);
+	}
+
+	var exec = require('child_process').exec,
+	child;
+
+	child = exec('git-annex import --json ' + fileName,
+		     function (error, stdout, stderr) {
+			 console.log(stdout);
+			 response.end(stdout)
+			 console.log(stderr);
+			 if (error !== null) {
+			     console.log('exec error: ' + error);
+			 }
+		     });
+
+    } catch (exception) {
+        response.send(404);
+    }
+    
+});
+
+
+app.post('/api/upload', function(req, res) {
+ 
+    var serverPath = '/images/' + req.files.userPhoto.name;
+    console.log(serverPath);
+    require('fs').rename(
+	req.files.userPhoto.path,
+	'/home/befree/dev/baobaxia' + serverPath,
+	function(error) {
+	    if(error) {
+		res.send({
+		    error: 'Ah crap! Something bad happened'
+		});
+		return;
+	    }
+ 
+	    res.send({
+		path: serverPath
+	    });
+	}
+    );
+});
+
+
 app.listen(9080);
+
+function publish_media(JSON){
+    
+    var path = request.body.path;
+    var fileName = request.body.filename;
+        
+    var exec = require('child_process').exec, 
+    child;
+    
+    child = exec('git-annex import --json ' + path + fileName,
+		 function (error, stdout, stderr) {
+		     console.log(stdout);
+		     response.end(stdout)
+		     console.log(stderr);
+		     if (error !== null) {
+			 console.log('exec error: ' + error);
+		     }
+		 });
+    
+}
+
+function getExtension(filename) {
+    var i = filename.lastIndexOf('.');
+    return (i < 0) ? '' : filename.substr(i);
+}
+
--- tester_node.js
+++ tester_node.js
@@ -1,36 +1,22 @@
 var request = require('request');
 
-request.post({
-    url: 'http://localhost:9080/api/view',
+request.get({
+    url: 'http://localhost:9080/api/view/whereis',
     headers: {
 	'Content-Type': 'application/json; charset=utf-8'
     },
     body: JSON.stringify({
 	path : '/home/befree/annex/redemocambos/mocambolas/vince/',
-	filename: 'esquema_infra.pdf'
+	filename: "'Altakamul Terra Terra.mp4'"
     })
 }, function(error, response, body){
+    debugger;
+    //console.log('Erro:' + error + ' Code:' + response.statusCode);
     if (!error && response.statusCode == 200){
 	console.log(body)
+	
     }    
-//    response.on('end', function(){
-//	console.log(JSON.parse(body))
-//    });
-    
 });
 	     
-
-
-    // response.on("data", function(chunk) {
-    //     console.log(chunk);
-    // });
-
-    // response.end("end", function() {
-    //     console.log("end");
-    // });
-
-
-
-
 
     
