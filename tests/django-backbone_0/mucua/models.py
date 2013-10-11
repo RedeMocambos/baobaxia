e412e1d94b33f022661de947d7449bac1d887b86
Fernao Lopes <fernao@riseup.net>
2013-10-11 18:12:46 -0300

    pegando mucuas do git annex

--- tests/django-backbone_0/mucua/models.py
+++ tests/django-backbone_0/mucua/models.py
@@ -6,7 +6,7 @@ from django.conf import settings
 #from repository.models import gitAnnexStatus
 from django.contrib.auth.models import User
 from bbx.settings import DEFAULT_REPOSITORY
-from django.db.models.loading import get_model
+from django.db.models import get_model
 
 # MUCUA_NAME_UUID é uma tupla com nome e uuid da mucua (pode ler do settings.py)
 
@@ -17,20 +17,22 @@ MUCUA_NAME_UUID = [ ('a30a926a-3a8c-11e2-a817-cb26bd9bc8d3','dandara'), ('049262
 def getAvailableMucuas():
     """Get a list of available policies from POLICIES_DIR."""
     import json
-#    from repository.models import Repository, gitAnnexStatus
-#    print Repository
-#    repository = Repository.objects.get(repositoryName = DEFAULT_REPOSITORY)
-#    jsonRepositoryStatus = json.loads(gitAnnexStatus(repository.repositoryURLOrPath))
+    from repository.models import Repository    
+    
+    repository = Repository.objects.get(repositoryName = DEFAULT_REPOSITORY)
+    jsonRepositoryStatus = json.loads(gitAnnexStatus(repository.repositoryURLOrPath))
+    
+    # return [ ('a30a926a-3a8c-11e2-a817-cb26bd9bc8d3','dandara'), ('0492621a-4195-11e2-b8c7-43de40a4e11c','acotirene') ]
+    
     # TODO HIGH: Colocar trusted..
-#    return [( mucua.description, mucua.uuid ) for mucua in  jsonRepositoryStatus.__dict__['semitrusted repositories']] 
-    return [ ('a30a926a-3a8c-11e2-a817-cb26bd9bc8d3','dandara'), ('0492621a-4195-11e2-b8c7-43de40a4e11c','acotirene') ]
+    return [( mucua.description, mucua.uuid ) for mucua in  jsonRepositoryStatus.__dict__['semitrusted repositories']] 
 
 
 class Mucua(models.Model):
     description = models.CharField(max_length=100, editable=False)
     note = models.TextField(max_length=300, blank=True)
     uuid = models.CharField(max_length=36, choices=getAvailableMucuas(), default='dandara')
-    repository = models.ManyToManyField('repository.Repository', related_name='mucuas', blank=True, null=True)
+    repository = models.ManyToManyField('repository.Repository', related_name='mucuas')
     mocambolas = models.ManyToManyField(User, through='mocambola.Mocambola', related_name='mucuas')
 
     def getDescription(self):
