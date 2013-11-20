6ff1a6c6cc490dd425a2d8829a0a451ed40b9375
Fernão Lopes <fernao@riseup.net>
2013-11-20 17:35:30 -0200

    adicionando rota /[repository]/mucuas para pegar mucuas do [repository]

--- tests/django-backbone_0/mucua/urls.py
+++ tests/django-backbone_0/mucua/urls.py
@@ -4,6 +4,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = patterns('mucua.views',
     url(r'^mucua/list', 'mucua_list'),
     url(r'^mucua/', 'mucua_get_default'),
+    url(r'^(?P<repository>\w+)/mucuas', 'mucua_list'),
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- tests/django-backbone_0/mucua/views.py
+++ tests/django-backbone_0/mucua/views.py
@@ -5,15 +5,26 @@ from django.shortcuts import get_object_or_404, render, render_to_response, redi
 from django.http import HttpResponseRedirect, HttpResponse
 from bbx.settings import DEFAULT_MUCUA
 from mucua.models import Mucua,getAvailableMucuas,getDefaultMucua
+from repository.models import Repository
 from mucua.serializers import MucuaSerializer
 
 @api_view(['GET'])
-def mucua_list(request):
+def mucua_list(request, repository = None):
     """
     List all mucuas
     """    
-    mucuas = getAvailableMucuas()   # retorna tupla de mucuas
+    if repository:
+        try:
+            repository = Repository.objects.get(name = repository)
+        except Repository.DoesNotExist:
+            return Response("Repository not found")
+            
+    mucuas = getAvailableMucuas(None, repository)   # retorna tupla de mucuas
     mucuas_list = []
+    
+    if mucuas is None:
+        return Response(None)
+    
     for mucua_obj in mucuas:
         mucua_note = mucua_obj[1]
         
@@ -22,7 +33,7 @@ def mucua_list(request):
             
         except Mucua.DoesNotExist:
             print "not found: ", mucua_note
-            mucua = False
+            #return Response("Mucua not found")
         
         if mucua:
             mucuas_list.append(mucua)
