089d228d58719241c94601764dd0af5969199c19
20 <vince@mocambos.net>
2014-05-24 15:48:45 +0200

    Atualidano o settings example

--- app/django-bbx/bbx/settings.example.py
+++ app/django-bbx/bbx/settings.example.py
@@ -11,7 +11,8 @@ PROJECT_ROOT = os.path.realpath(
 )
 
 # Change this to the full path to your own repository
-REPOSITORY_DIR = "/data"
+REPOSITORY_DIR_NAME ="repositories" 
+REPOSITORY_DIR = os.path.join(MEDIA_ROOT, REPOSITORY_DIR_NAME)
 MOCAMBOLA_DIR = "mocambolas"  # Nome da pasta onde estao os usuarios em .json
 DEFAULT_MUCUA = "dandara"
 DEFAULT_REPOSITORY = "mocambos"
@@ -68,7 +69,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = os.path.join(REPOSITORY_DIR, 'repositories')
+MEDIA_ROOT = '/data/baobaxia/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -79,7 +80,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.join(REPOSITORY_DIR, 'static')
+STATIC_ROOT = os.path.join(MEDIA_ROOT, 'static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
