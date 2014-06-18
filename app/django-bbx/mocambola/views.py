d3e3dcab8a076107829f1e7dc0230e1c4facb5e9
fernao <fernao@riseup.net>
2014-06-18 19:22:41 -0300

    merge

--- app/django-bbx/mocambola/views.py
+++ app/django-bbx/mocambola/views.py
@@ -1,10 +1,13 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
+from django.core.context_processors import csrf
+from django.template import Template, RequestContext
 
 from mocambola.serializers import UserSerializer
-
+from bbx.auth import FileBackend
 
 @api_view(['GET'])
 def mocambola_list(request, repository, mucua):
@@ -29,3 +32,33 @@ def mocambola_detail(request, repository, mucua, mocambola):
     # serializa e da saida
     serializer = UserSerializer(user)
     return Response(serializer.data)
+
+@api_view(['GET', 'POST'])
+def login(request):
+    print request.method
+    if request.method == 'GET':
+        # gera token para tela de login
+        c = RequestContext(request, {'autoescape': False})
+        c.update(csrf(request))
+        t = Template('{ "csrftoken": "{{ csrf_token  }}" }')
+        return HttpResponse(t.render(c), mimetype=u'application/json')
+        
+    elif request.method == 'POST':
+        username = request.DATA['username'] + '@' + request.DATA['mucua'] + '.' + request.DATA['repository'] + '.net'
+        password = request.DATA['password']
+        fileBackend = FileBackend()
+        authenticate = fileBackend.authenticate(username, password)
+        if (authenticate):
+            try:
+                user = User.objects.get(username=username)
+            except User.DoesNotExist:
+                logger.debug(u"%s" % (
+                        _('Exception caught, UserDoesNotExist')
+                        ))                
+            if user:
+                serializer = UserSerializer(user)
+                return Response(serializer.data)
+            else:
+                return Response('Usuario inexistente.')
+        else:
+                return Response('Usuario ou senha invalid@s.')
