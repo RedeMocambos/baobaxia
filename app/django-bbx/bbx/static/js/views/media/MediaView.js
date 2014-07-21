ce41ec8b3c41964808174cf1a6b3f398194fedc7
fernao <fernao@riseup.net>
2014-07-21 11:31:27 -0300

    alteracoes nara na MediaView: tags e solicitar copia

--- app/django-bbx/bbx/static/js/views/media/MediaView.js
+++ app/django-bbx/bbx/static/js/views/media/MediaView.js
@@ -25,7 +25,6 @@ define([
 		data.media = data.medias;
 		data.config = config;
 		data.baseUrl = BBXBaseFunctions.getDefaultHome();
-		
 		$('#back-to-results').html("<a class='back-to-results' href='javascript: history.back(-1)'><img src='" + config.imagePath + "/voltar.png'> voltar para a busca</a>");
 		$('#content').html(_.template(MediaViewTpl, data));
 		
