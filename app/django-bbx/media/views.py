7ffafab3955678245418409375f7f718755282af
fernao <fernao@riseup.net>
2014-09-23 19:58:39 -0300

    woops

--- app/django-bbx/media/views.py
+++ app/django-bbx/media/views.py
@@ -167,7 +167,7 @@ def media_list(request, repository, mucua, args=None, format=None):
         if (len(term_sql) > 0):
             term_sql = ' AND (' + term_sql + ')'
                             
-        sql = "SELECT DISTINCT m.* FROM media_media m LEFT JOIN media_media_tags mt ON m.id = mt.media_id LEFT JOIN tag_tag t ON mt.tag_id = t.id  WHERE (" + origin_sql + " OBrepository_id = ? ) " + term_sql + " ORDER BY " + ordering_sql + " LIMIT ? "
+        sql = "SELECT DISTINCT m.* FROM media_media m LEFT JOIN media_media_tags mt ON m.id = mt.media_id LEFT JOIN tag_tag t ON mt.tag_id = t.id  WHERE (" + origin_sql + " repository_id = ? ) " + term_sql + " ORDER BY " + ordering_sql + " LIMIT ? "
         sql = sql.decode('utf-8')
         
         params.extend(ordering_params)
