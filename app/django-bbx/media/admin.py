c72118301c2991cbcda12eae06975c9ca8df1f0a
Vince <vince@mocambos.net>
2015-05-05 00:30:43 -0300

    Busca melhorada no quintal/admin

--- app/django-bbx/media/admin.py
+++ app/django-bbx/media/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from media.models import Media
 
-admin.site.register(Media)
+class MediaAdmin(admin.ModelAdmin):
+    list_display = ('name','origin','date','author','format')
+    search_fields = ('name',)
+    list_filter = ('date', 'origin', 'author', 'format', 'is_local', 'is_requested')
+
+admin.site.register(Media, MediaAdmin)
